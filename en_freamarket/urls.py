54651d40a3a7ce777e18eae1ca42e95978804005
kana <matsuo@blayn.co.jp>
2025-04-14 10:17:51 +0900

    アカウントのログインページで「ログインページ」と表示

--- en_freamarket/urls.py
+++ en_freamarket/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('', include('en_freamarket.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('freamarket/', include('freamarket.urls')),
 ]
