ee5b467b3a446c3b045101f79293a97793cc3f6a
kana1018w <kana1018matsuo@gmail.com>
2025-05-18 22:18:53 +0900

    Create portfolio

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
+    path('about/', views.about, name='about'),
     path('terms/', views.terms, name='terms'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 def portfolio_view(request):
     return render(request, 'portfolio.html')
 
+# アプリ概要
+def about(request):
+    return render(request, 'core/about.html')
+
 # ご利用規約
 def terms(request):
     return render(request, 'core/terms.html')
