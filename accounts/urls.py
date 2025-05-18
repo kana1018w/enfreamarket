f19fb13c0f21f947583d9355f3ec9b86a8c5dcc1
kana1018w <kana1018matsuo@gmail.com>
2025-05-18 14:22:32 +0900

    Cange component for mypage menu

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
     # maypage
     path('mypage/', views.mypage, name='mypage'),
     path('mypage/listings/',views.my_listings, name='my_listings'),
-    path('mypage/intents/given/',views.my_intents_given, name='my_intents_given'),
-    path('mypage/intents/received/',views.my_intents_received, name='my_intents_received'),
     path('profile/name/edit/', views.profile_name_edit, name='profile_name_edit'),
     path('profile/display_name/edit/', views.profile_display_name_edit, name='profile_display_name_edit'),
     path('profile/email/edit/', views.profile_email_edit, name='profile_email_edit'),
